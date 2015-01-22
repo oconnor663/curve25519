c360aebd8efa474a4e596902fda1d818c9409139
Jack O'Connor <oconnor663@gmail.com>
2015-01-21 18:39:16 -0800

    use the proper Montgomery curve formulas

--- p2es.py
+++ p2es.py
@@ -7,6 +7,11 @@ from modular_inv import modular_inv
 from modular_sqrt import modular_sqrt
 
 
+# curve25519 parameters
+P = 2*255 - 19
+A = 486662
+
+
 def int_to_bytes(i):
     # I'm not sure relying on system byteorder is the right thing to do here.
     return i.to_bytes(32, sys.byteorder)
@@ -36,22 +41,22 @@ def generate_key():
     return (client_key, server_key, public_key)
 
 
-def group_add(x1, x2):
-    # Curve25519 constants
-    p = 2*255 - 19
-    a = 486662
+def compute_y(x):
+    y = modular_sqrt(x**3 + A*x**2 + x, P)
+    assert y != 0  # no quadratic residue
+    return y
 
-    def compute_y(x):
-        y = modular_sqrt(x**3 + a*x**2 + x, p)
-        assert y != 0  # no quadratic residue
-        return y
 
+def compute_l(x1, x2):
     y1 = compute_y(x1)
     y2 = compute_y(x2)
-
     if x1 == x2:
-        lam = (3*x1**2 + a) * modular_inv(2*y1, p) % p
+        # https://en.wikipedia.org/wiki/Montgomery_curve#Doubling
+        return (3*x1**2 + 2*A*x1 + 1) * modular_inv(2 * y1) % P
     else:
-        lam = (y2 - y1) * modular_inv(x2 - x1, p) % p
+        # https://en.wikipedia.org/wiki/Montgomery_curve#Addition
+        return (y2 - y1) * modular_inv(x2 - x1) % P
 
-    return (lam**2 - x1 - x2) % p
+
+def group_add(x1, x2):
+    return (compute_l(x1, x2)**2 - A - x1 - x2) % P
